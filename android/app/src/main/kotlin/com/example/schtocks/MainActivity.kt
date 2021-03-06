13f9ef10848d494222a2b3b31e3f1c7607fd93c5
dhruvweaver <dhruv.weaver@gmail.com>
2021-03-05 23:43:56 -0500

    Wrote out home page in prep for stock widgets

--- android/app/src/main/kotlin/com/example/schtocks/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/schtocks/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.example.schtocks
 
+import androidx.annotation.NonNull;
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+    override fun onPostResume() {
+        super.onPostResume()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.navigationBarColor = 0
+      }
 }
