c5b89ea17f084d63164f7066312dd0f982daa526
Ztaylor3812 <77793998+Ztaylor3812@users.noreply.github.com>
2021-03-06 23:33:39 -0500

    Updated GraphBig and GraphSmall to take parameters of type List<FlSpot>

--- lib/widgets/graph_small.dart
+++ lib/widgets/graph_small.dart
@@ -5,45 +5,36 @@ import 'package:fl_chart/fl_chart.dart';
 import 'dart:math';
 
 class GraphSmall extends StatefulWidget {
+   final List<FlSpot> spots;
+  GraphSmall({
+    List<FlSpot> spots
+  }): this.spots = spots;
+  
   @override
-  _GraphSmallState createState() => new _GraphSmallState();
+  _GraphSmallState createState() => new _GraphSmallState(spots);
 }
 
 class _GraphSmallState extends State<GraphSmall> { 
-  int _counter = 0;
+  _GraphSmallState(this.spots);
+  final List<FlSpot> spots;
+  
   Color col = Colors.red;
-  var _values = <int>[0];
-  var _spots = <FlSpot>[FlSpot(0, 0)];
-  var rng = new Random();
 
-  void incrementCounter() {
-    setState(() {
-      _counter++;
-      _values.add(_values.last + rng.nextInt(1000) - 500);
-      if (_values.last < 0) {
-        _values.last = 0;
-      }
-      _spots.add(FlSpot(_counter.toDouble(), _values.last.toDouble()));
-      if (_values[_counter] > _values[_counter - 1]) {
-        col = Colors.green;
-      } else {
-        col = Colors.red;
-      }
-    });
+  void initializeColor() {
+    if (spots.length > 1 && spots[spots.length].y > spots[spots.length-1].y){
+      col = Colors.green;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    for (int i = 0; i < 40; i++) {
-      incrementCounter();
-    }
+    
     return new LineChart(LineChartData(
         titlesData: FlTitlesData(
             bottomTitles: SideTitles(
               showTitles: false,
             ),
             leftTitles: SideTitles()),
-        minX: (_counter - 40).toDouble(),
         minY: 0,
         clipData:
             FlClipData(left: true, right: true, top: false, bottom: true),
@@ -62,7 +53,7 @@ class _GraphSmallState extends State<GraphSmall> {
               isCurved: true,
               curveSmoothness: 0.1,
               barWidth: 3,
-              spots: _spots,
+              spots: spots,
               dotData: FlDotData(show: false))
         ]));
   }
